3c32192d78b2baed0400d17764ee9763e47215ea
Khalil <khalil.bm@hotmail.com>
2026-07-22 16:01:41 -0300

    feat: add totals dto models

---
+++ Backend/DTOs/TotalGeralDto.cs
@@ -0,0 +1,8 @@
+namespace Backend.DTOs;
+
+public class TotalGeralDto
+{
+    public decimal TotalReceitas { get; set; }
+    public decimal TotalDespesas { get; set; }
+    public decimal Saldo { get; set; }
+}
