6c270177592ef0604dae2ed8ee7dbad0798ce229
Khalil <khalil.bm@hotmail.com>
2026-07-22 15:02:26 -0300

    feat: create initial database migration

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a21aea9b475291213e276e88fb14114fe46b45b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72765e4773f5f038133437aead139d42e7dc85ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
