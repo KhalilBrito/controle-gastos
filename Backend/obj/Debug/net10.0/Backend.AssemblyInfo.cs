4746718fbd81d2703513092dd6a0c244839456a2
Khalil <khalil.bm@hotmail.com>
2026-07-22 23:18:16 -0300

    feat: finish MVP

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109aac560a2c49025866ea9d5965f37740803a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9e52210d65563aafaf413519a53316458d5c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
